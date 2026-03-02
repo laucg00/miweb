12c2ac575ef7d959d2f6cc810d14a3af62eb6924
[Laura Cuñado García] <[lcunado@uoc.edu]>
2026-03-02 11:02:27 +0100

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component'; 
+import { HeroComponent } from './components/hero/hero.component'; 
+import { ServicesComponent } from './components/services/services.component'; 
+import { AboutComponent } from './components/about/about.component'; 
+import { ContactComponent } from './components/contact/contact.component'; 
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [ HeaderComponent, HeroComponent, ServicesComponent, AboutComponent, ContactComponent, FooterComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
